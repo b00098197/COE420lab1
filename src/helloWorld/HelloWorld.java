146c16470da4f3e8680e9fce869e285f6e6e9fe3
Ali Amiri <b00095121@aus.edu>
2026-01-22 23:08:53 +0400

    Feature B added

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -3,5 +3,9 @@ package helloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        featureB();
+    }
+    public static void featureB() {
+        System.out.println("Feature B added by member_B");
     }
 }
